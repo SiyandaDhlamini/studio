a34b0dca95673f383f2e3401e163fe5e038b1b12
Siyanda Dhlamini <siyandanxele68@gmail.com>
2025-09-17 13:22:15 +0000

    On '"view certificate" for AI Essentials link this (https://www.coursera

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -123,6 +123,6 @@ export const certifications: Certification[] = [
     name: 'AI Essentials',
     period: '',
     description: 'Through the AI Essentials program, I gained practical knowledge and experience in Artificial Intelligence (AI) and Machine Learning (ML), including deep learning, generative AI, and automation. I also developed strong skills in data analysis and explored the application of AI in B2B sales and business contexts. This training has provided me with a solid foundation to apply AI/ML techniques in solving real-world problems and building intelligent solutions.',
-    pdfUrl: '#',
+    pdfUrl: 'https://www.coursera.org/account/accomplishments/records/PWSKBGRX13BB',
   },
 ];
